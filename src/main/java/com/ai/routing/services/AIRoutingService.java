8d9afa8ee2ddc539c84b62b41d1ac88ddeb3d2f5
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-22 13:44:49 +0100

    BetaDistribution added to suggewt right PSP -> 2

--- src/main/java/com/ai/routing/services/AIRoutingService.java
+++ src/main/java/com/ai/routing/services/AIRoutingService.java
@@ -1,6 +1,10 @@
 package com.ai.routing.services;
 
+import com.ai.routing.model.BinPsp;
+import com.ai.routing.model.DataStorage;
+import com.ai.routing.model.History;
 import com.ai.routing.model.Point;
+import org.apache.commons.math3.distribution.BetaDistribution;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,12 +14,34 @@ import java.util.Set;
 @Service
 public class AIRoutingService {
 
+    private DataStorage dataStorage = new DataStorage();
+
+    private Float calculateExpectedProbability(String bin, String psp) {
+        BetaDistribution beta = this.getDecisionDistribution(bin, psp);
+        return (float) beta.sample();
+    }
+
+    private BetaDistribution getDecisionDistribution(String bin, String psp) {
+        History record = dataStorage.getHistory(new BinPsp(bin, psp));
+        return new BetaDistribution(record.getSuccessCount() + 1, record.getFailCount() + 1);
+    }
+
     public String suggestPSP(String bin) {
         Set<String> availablePSPs = this.getPSPs(bin);
         if (availablePSPs.isEmpty()) {
             return null;
         }
-        return null;
+        String bestPSP = "";
+        Float maxProbability = -1.0f;
+        Float expectedProbability;
+        for (String psp : availablePSPs) {
+            expectedProbability = this.calculateExpectedProbability(bin, psp);
+            if (expectedProbability >= maxProbability) {
+                bestPSP = psp;
+                maxProbability = expectedProbability;
+            }
+        }
+        return bestPSP;
     }
 
     private Set<String> getPSPs(String bin) {
