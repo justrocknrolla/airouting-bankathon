0517225e7e1814aefd5ac14b3907063c092fb250
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-22 13:30:00 +0100

    moar tests added

--- src/test/java/com/ai/routing/model/TestDataStorage.java
+++ src/test/java/com/ai/routing/model/TestDataStorage.java
@@ -60,4 +60,48 @@ public class TestDataStorage {
         assertEquals(1, successful);
         assertEquals(0, unsuccessful);
     }
+
+    @Test
+    public void testGetAllBinsWhenSuccessfulObservationPosted() {
+        storage.postResult(BIN1_PSP1, true);
+
+        Set<String> bins = storage.getAllBins(PSP1);
+        assertTrue(bins.contains(BIN1));
+    }
+
+    @Test
+    public void testGetAllPspsWhenSuccessfulObservationPosted() {
+        storage.postResult(BIN1_PSP1, true);
+
+        Set<String> psps = storage.getAllPsps(BIN1);
+        assertTrue(psps.contains(PSP1));
+    }
+
+    @Test
+    public void testGetHistoryWhenUnsuccessfulObservationPosted() {
+        storage.postResult(BIN1_PSP1, false);
+
+        History result = storage.getHistory(BIN1_PSP1);
+        int successful = result.getSuccessCount();
+        int unsuccessful = result.getFailCount();
+
+        assertEquals(0, successful);
+        assertEquals(1, unsuccessful);
+    }
+
+    @Test
+    public void testGetAllBinsWhenUnsuccessfulObservationPosted() {
+        storage.postResult(BIN1_PSP1, false);
+
+        Set<String> bins = storage.getAllBins(PSP1);
+        assertTrue(bins.contains(BIN1));
+    }
+
+    @Test
+    public void testGetAllPspsWhenUnsuccessfulObservationPosted() {
+        storage.postResult(BIN1_PSP1, false);
+
+        Set<String> psps = storage.getAllPsps(BIN1);
+        assertTrue(psps.contains(PSP1));
+    }
 }
