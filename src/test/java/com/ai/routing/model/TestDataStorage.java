390b41ae4d5c66b2a0a6e528b3151bf2f3587534
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-22 15:59:14 +0100

    more tests for DataStorage

--- src/test/java/com/ai/routing/model/TestDataStorage.java
+++ src/test/java/com/ai/routing/model/TestDataStorage.java
@@ -16,8 +16,15 @@ import static org.junit.Assert.assertTrue;
 public class TestDataStorage {
 
     private DataStorage storage;
+
     private final String BIN1 = "bin1";
+    private final String BIN2 = "bin2";
+    private final String BIN3 = "bin3";
+
     private final String PSP1 = "psp1";
+    private final String PSP2 = "psp2";
+    private final String PSP3 = "psp3";
+
     private final BinPsp BIN1_PSP1 = new BinPsp(BIN1, PSP1);
 
     @PostConstruct
@@ -103,5 +110,50 @@ public class TestDataStorage {
 
         Set<String> psps = storage.getAllPsps(BIN1);
         assertTrue(psps.contains(PSP1));
+    }
+
+    @Test
+    public void testGetHistoryWhenMultipleObservationPostedForSingleCombination() {
+
+        storage.postResult(BIN1_PSP1, true);
+        storage.postResult(BIN1_PSP1, false);
+        storage.postResult(BIN1_PSP1, true);
+        storage.postResult(BIN1_PSP1, false);
+        storage.postResult(BIN1_PSP1, true);
+        storage.postResult(BIN1_PSP1, false);
+        storage.postResult(BIN1_PSP1, false);
+
+        History result = storage.getHistory(BIN1_PSP1);
+        int successful = result.getSuccessCount();
+        int unsuccessful = result.getFailCount();
+
+        assertEquals(3, successful);
+        assertEquals(4, unsuccessful);
+    }
+
+    @Test
+    public void testGetHistoryWhenMultipleObservationPostedForDifferentPairs() {
+        storage.postResult(BIN1_PSP1, true);
+        storage.postResult(BIN1_PSP1, true);
+        storage.postResult(new BinPsp(BIN2, PSP2), false);
+        storage.postResult(new BinPsp(BIN2, PSP3), true);
+        storage.postResult(new BinPsp(BIN3, PSP3), true);
+
+        History result = storage.getHistory(new BinPsp(BIN2, PSP2));
+        int successful = result.getSuccessCount();
+        int unsuccessful = result.getFailCount();
+
+        assertEquals(0, successful);
+        assertEquals(1, unsuccessful);
+    }
+
+    @Test
+    public void testGetAllBinsWhenMultipleObservationPostedForDifferentPairs() {
+        storage.postResult(BIN1_PSP1, false);
+        storage.postResult(new BinPsp(BIN2, PSP1), false);
+
+        Set<String> bins = storage.getAllBins(PSP1);
+        assertTrue(bins.contains(BIN1));
+        assertTrue(bins.contains(BIN2));
     }
 }
