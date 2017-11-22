04bdc214cf47f80447e1a789cc6de90a2463aade
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-22 12:15:54 +0100

    dm ->  history further implementation.

--- src/main/java/com/ai/routing/model/DataStorage.java
+++ src/main/java/com/ai/routing/model/DataStorage.java
@@ -1,4 +1,12 @@
 package com.ai.routing.model;
 
+import java.util.HashMap;
+
 public class DataStorage {
+
+    private HashMap<BinPsp, History> history = new HashMap<>();
+
+    public History getHisotry(BinPsp binPsp) {
+        return history.getOrDefault(binPsp, new History());
+    }
 }
--- src/main/java/com/ai/routing/model/History.java
+++ src/main/java/com/ai/routing/model/History.java
@@ -1,4 +1,51 @@
 package com.ai.routing.model;
 
+import java.util.Objects;
+
 public class History {
+    private final int successCount;
+    private final int failCount;
+
+    public History() {
+        this(0, 0);
+    }
+
+    public History(int successCount, int failCount) {
+        this.successCount = successCount;
+        this.failCount = failCount;
+    }
+
+    public History addSuccess() {
+        return new History(successCount + 1, failCount);
+    }
+
+    public History addFail() {
+        return new History(successCount, failCount + 1);
+    }
+
+    public History addResult(boolean result) {
+        return result ? addSuccess() : addFail();
+    }
+
+    public int getSuccessCount() {
+        return successCount;
+    }
+
+    public int getFailCount() {
+        return failCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        History history = (History) o;
+        return successCount == history.successCount &&
+                failCount == history.failCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(successCount, failCount);
+    }
 }
