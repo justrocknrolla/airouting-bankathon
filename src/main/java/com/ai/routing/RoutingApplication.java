a7cc70cbce5cf13ca633341e8071be231cce719a
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-22 16:49:05 +0100

    HistoryImporterService bounded to spring boot context

--- src/main/java/com/ai/routing/RoutingApplication.java
+++ src/main/java/com/ai/routing/RoutingApplication.java
@@ -1,5 +1,7 @@
 package com.ai.routing;
 
+import com.ai.routing.services.HistoryImporterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +9,14 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.ai.routing")
 public class RoutingApplication {
+
+	private final HistoryImporterService historyImporterService;
+
+	@Autowired
+	public RoutingApplication(HistoryImporterService historyImporterService) {
+		this.historyImporterService = historyImporterService;
+		historyImporterService.importData(RoutingApplication.class.getResourceAsStream("/data.txt"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoutingApplication.class, args);
