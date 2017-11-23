a2d88a94a94de785217893a33dd2b48662e84050
sviridenich <sergey.sviridenko@gmail.com>
2017-11-23 06:09:57 -0500

    tuning data a little bit

--- python/generate_data.bat
+++ python/generate_data.bat
@@ -1,2 +1,2 @@
-#python main.py 0.5 2 0.5 2 0.5 2 0.5 2 0.5 2 0.5 2 > ../src/main/resources/initial_data.txt
-python main.py 0.2 60 0.8 10 0.5 10 0.9 60 0.8 10 0.2 10 > ../src/main/resources/trained_data.txt
+python main.py 0.5 2 0.5 2 0.5 2 0.5 2 0.5 2 0.5 2 > ../src/main/resources/initial_data.txt
+python main.py 0.2 60 0.8 10 0.7 20 0.9 60 0.8 10 0.2 10 > ../src/main/resources/trained_data.txt
