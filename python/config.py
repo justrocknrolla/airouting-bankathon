42621d1bf8ad03fc956fa874fb82c70223fbcd58
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-11-23 01:37:08 +0100

    python rfct

--- python/config.py
+++ python/config.py
@@ -1,6 +1,6 @@
 
 
-PSP_LIST = ["psp1", "psp2"]
+PSP_LIST = ["adyen", "wirecard"]
 BIN_LIST = ["bin1", "bin2", "bin3"]
 BIN_PSP_PAIRS = len(PSP_LIST) * len(BIN_LIST)
 REQUIRED_NUMBER_OF_ARGUMENTS = 2 * BIN_PSP_PAIRS + 1
